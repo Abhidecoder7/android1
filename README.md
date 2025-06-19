5ef8f1c1cad35772968638dc916cb17816771a91
abhishek.vishwakarma <abhishek.vish.code@gmail.com>
2025-06-19 13:50:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CT_Post_Initialization
--- app/src/main/java/com/example/demo/MyApplication.java
+++ app/src/main/java/com/example/demo/MyApplication.java
@@ -1,22 +1,4 @@
 package com.example.demo;
 
-import android.app.Application;
-import com.clevertap.android.sdk.ActivityLifecycleCallback;
-import com.clevertap.android.sdk.CleverTapAPI;
-import com.clevertap.android.sdk.interfaces.NotificationHandler;
-import com.clevertap.android.pushtemplates.PushTemplateNotificationHandler;
-
-public class MyApplication extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        // Initialize CleverTap
-        ActivityLifecycleCallback.register(this);
-
-        // Set the notification handler for push templates
-        CleverTapAPI.setNotificationHandler((NotificationHandler)new PushTemplateNotificationHandler());
-
-        // Debug level is already set in MainActivity, so no need to set it here
-    }
+public class MyApplication {
 }
